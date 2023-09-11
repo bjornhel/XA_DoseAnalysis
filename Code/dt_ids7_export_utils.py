2f210c70daac0c7e054f7d89ece2c4c99a1654cb
BH <bjornhel@gmail.com>
2023-09-11 15:06:02 +0200

    Added Gitignore

--- Code/dt_ids7_export_utils.py
+++ Code/dt_ids7_export_utils.py
@@ -5,3 +5,7 @@ The following functions are included in this module:
 
 import pandas as pd
 
+def filter_NaT(df_ids7):
+    """ This function removes row with NaT in the column 'Bestilt dato og tidspunkt'"""
+    df_ids7 = df_ids7[df_ids7['Bestilt dato og tidspunkt'].notnull()]
+    return df_ids7
