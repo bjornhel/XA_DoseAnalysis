01d6f6c6e8150d22ebe971fac614fa429cd4855d
BH <bjornhel@gmail.com>
2023-09-12 11:06:32 +0200

    Added functionality: checking Accession no format

--- Code/dt_ids7_export_utils.py
+++ Code/dt_ids7_export_utils.py
@@ -4,8 +4,32 @@ The following functions are included in this module:
 """
 
 import pandas as pd
+import re
 
 def filter_NaT(df_ids7):
-    """ This function removes row with NaT in the column 'Bestilt dato og tidspunkt'"""
+    """
+    This function removes row with NaT in the column 'Bestilt dato og tidspunkt
+    """
     df_ids7 = df_ids7[df_ids7['Bestilt dato og tidspunkt'].notnull()]
     return df_ids7
+
+def check_accession_no(df_ids7, verbose=False):
+    """
+    This function checks if the accession number has a correct start and length.
+    The currently allowed accession numbers are:
+    (NORRH|NRRH|NRAK|NIRH|NKRH|NNRH|NRRA)
+    Additionally the length of the accession number must be 16 characters.
+    """
+    valid_formats = r'^(NORRH|NRRH|NRAK|NIRH|NKRH|NNRH|NRRA)'
+    patten = re.compile(valid_formats)
+
+    is_valid_format = (df_ids7['Henvisnings-ID'].str.match(patten)) & (df_ids7['Henvisnings-ID'].str.len() == 16)
+
+    if verbose:
+        print('Number of rows with invalid accession number: {}'.format(sum(~is_valid_format)))
+        # Print the invalid accession numbers:
+        if sum(~is_valid_format) > 0:
+            print(df_ids7[~is_valid_format]['Henvisnings-ID'])
+    
+    return df_ids7[is_valid_format]
+    
