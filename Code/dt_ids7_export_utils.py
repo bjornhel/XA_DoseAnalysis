faf3f4dd3e40997051c15f2991b46fba3c3279d8
Bjørn Helge Østerås <bjornhel@gmail.com>
2023-09-13 15:16:58 +0200

    Added functions for errorchecking data

--- Code/dt_ids7_export_utils.py
+++ Code/dt_ids7_export_utils.py
@@ -6,11 +6,34 @@ The following functions are included in this module:
 import pandas as pd
 import re
 
-def filter_NaT(df_ids7):
+def filter_NaT(df_ids7, verbose=False):
     """
     This function removes row with NaT in the column 'Bestilt dato og tidspunkt'
     """
+    if verbose:
+        print('Number of rows with NaT in the column "Bestilt dato og tidspunkt": {}'.format(sum(df_ids7['Bestilt dato og tidspunkt'].isnull())))
+
     df_ids7 = df_ids7[df_ids7['Bestilt dato og tidspunkt'].notnull()]
+    return df_ids7
+
+def filter_cancelled(df_ids7, verbose=False):
+    """ 
+    This function removes rows where the procedures have been cancelled.
+    """
+    if verbose:
+        print('Number of cancelled procedures: {}'.format(sum(df_ids7['Avbrutt'] == 'Avbrutt')))
+
+    df_ids7 = df_ids7[df_ids7['Avbrutt'] != 'Avbrutt']
+    return df_ids7
+
+def filter_filter_phantom_etc(df_ids7, verbose=False):
+    """ 
+    This function removes rows representing non-human subjects (phantoms, animals, etc.).
+    """
+    if verbose:
+        print('Number of non-human subjects: {}'.format(sum(df_ids7['Henvisningskategori (RIS)'] == 'X Fantom/objekt/dyr/test')))
+
+    df_ids7 = df_ids7[df_ids7['Henvisningskategori (RIS)'] == 'X Fantom/objekt/dyr/test']
     return df_ids7
 
 def check_accession_format(df_ids7, verbose=False):
@@ -50,3 +73,73 @@ def check_accession_ids7_vs_dt(df_ids7, df_dt, verbose=False):
         print('Number of accession numbers in IDS7 not in DoseTrack: {}'.format(len(df_ids7[df_ids7['Henvisning_i_dt'] == False]['Henvisnings-ID'].drop_duplicates())))
 
     return df_ids7
+
+
+
+# Utility functions primarily used to check the data for abnormalities:
+def check_patents_with_multiple_bookings_on_same_time_with_different_accession(df_ids7):
+    """
+    This function is used to report whether there are patients with multiple bookings on the
+    same time with different accession numbers. This is useful in order to check whether there is a large 
+    number of patients with multiple rows of data that must be merged.
+    A previous run on over 4000 lines of PACS data revealed 20 cases of different accession no on the same time.
+    Many of these were cancelled procedusres, and should be removed in data filtration.
+    Others were infact the same procedure. These shoudl have their accession number changed to the one
+    reported in the dosetrack data.
+    """
+
+    # Make a list of all patients with multiple bookings on the same day with different accession numbers:
+    patient_list = df_ids7['Pasient'].drop_duplicates()
+    for patient in patient_list:
+        # Get the booking times for this patient:
+        booking_times = df_ids7[df_ids7['Pasient'] == patient]['Bestilt dato og tidspunkt']
+        # Sort the booking times:
+        booking_times = booking_times.sort_values()
+        # if there is more than one booking:
+        if len(booking_times) > 1:
+            # loop through all bookings:
+            for i in range(len(booking_times)-1):
+                # Check if the booking times are on the same day but different time with different accesstion numbers:
+                if (booking_times.iloc[i] == booking_times.iloc[i+1]):
+                    # Get the accession numbers:
+                    acc_no = df_ids7[(df_ids7['Pasient'] == patient) & (df_ids7['Bestilt dato og tidspunkt'] == booking_times.iloc[i])]['Henvisnings-ID'].drop_duplicates()
+                    # Check if there are multiple accession numbers:
+                    if len(acc_no) > 1:
+                        print('Patient: ' + str(patient) + ' has multiple accession numbers at ' + str(booking_times.iloc[i]) + ':')
+                        print(acc_no)
+                        print('')
+
+
+
+
+
+def check_patents_with_multiple_bookings_on_same_day_with_different_accession(df_ids7):
+    """
+    This function is used to report whether there are patients with multiple bookings on the
+    same day (not on the same time, as they are included in the data curation) with
+    different accession numbers. This is useful in order to check whether there is a large 
+    number of patients with multiple rows of data that must be merged.
+    On an earlier run with 4000 lines from the PACS only two cases was found.
+    Both cases included cancelled procedures.
+    """
+
+    # Make a list of all patients with multiple bookings on the same day with different accession numbers:
+    patient_list = df_ids7['Pasient'].drop_duplicates()
+    for patient in patient_list:
+        # Get the booking times for this patient:
+        booking_times = df_ids7[df_ids7['Pasient'] == patient]['Bestilt dato og tidspunkt']
+        # Sort the booking times:
+        booking_times = booking_times.sort_values()
+        # if there is more than one booking:
+        if len(booking_times) > 1:
+            # loop through all bookings:
+            for i in range(len(booking_times)-1):
+                # Check if the booking times are on the same day but different time with different accesstion numbers:
+                if (booking_times.iloc[i].date() == booking_times.iloc[i+1].date()) & (booking_times.iloc[i].time() != booking_times.iloc[i+1].time()):
+                    # Get the accession numbers:
+                    acc_no = df_ids7[(df_ids7['Pasient'] == patient) & (df_ids7['Bestilt dato og tidspunkt'] == booking_times.iloc[i])]['Henvisnings-ID'].drop_duplicates()
+                    # Check if there are multiple accession numbers:
+                    if len(acc_no) > 1:
+                        print('Patient: ' + str(patient) + ' has multiple accession numbers at ' + str(booking_times.iloc[i].date()) + ':')
+                        print(acc_no)
+                        print('')
