909155c4ddcce8249fe9917f8dc8d7f5b0b74159
BH <bjornhel@gmail.com>
2024-01-23 08:56:40 +0100

    Finished accounting for min and sec

--- Code/reporting_module.py
+++ Code/reporting_module.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-def _calc_ci(data_vector, ci = 95, n = 1000):
+def _calc_ci(data_vector, ci = 95, n = 10000):
     """
     This function calculated the confidence interval of the median of the data_vector.
     The default number of bootstrap samples is 1000.
@@ -20,6 +20,22 @@ def _calc_ci(data_vector, ci = 95, n = 1000):
     # Return the median and the confidence interval:
     return ci.quantile(0.025), ci.quantile(0.975)
 
+def _format_min_sec(data_vector):
+    """
+    This function formats the data_vector from seconds to minutes and seconds.
+    Then the function returns the minutes and seconds as strings.
+    If there is less than 10 seconds the function returns a string with a leading zero.
+    """
+    minutes, seconds = divmod(data_vector, 60)
+    minutes = str(round(minutes))
+    seconds = round(seconds)
+    if seconds < 10:
+        seconds = '0' + str(round(seconds))
+    else:
+        seconds = str(round(seconds))
+    return minutes, seconds
+
+
 def print_obs_per_lab(data):
     for lab in data['Modality Room'].unique():
         print(lab + ': n = ' + str(len(data[data['Modality Room'] == lab])))
@@ -34,11 +50,11 @@ def print_summary_per_lab(data, ci = False):
         if ci:
             lci, uci = _calc_ci(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'])
         print(lab + ': n = {:4}'.format(len(data[data['Modality Room'] == lab])) + \
-              ', DAP: Median - ' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].median(), 2)) + ',' + \
-              (' 95% CI: [' + str(round(lci, 2)) + ' - ' + str(round(uci, 2)) + '] (Gy*cm2)' if ci else '') + \
+              ', DAP: Median - ' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].median(), 2)) + ' (Gy*cm2),' + \
+              (' 95% CI: [' + str(round(lci, 2)) + ' - ' + str(round(uci, 2)) + ']' if ci else '') + \
               # 25 th percentile:
               ' IQR [' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].quantile(0.25), 2)) + \
-              ' - ' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].quantile(0.75), 2)) + '] (Gy*cm2), ' + \
+              ' - ' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].quantile(0.75), 2)) + '], ' + \
               'Range (' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].min(), 2)) + \
               ' - ' + str(round(data[data['Modality Room'] == lab]['DAP Total (Gy*cm2)'].max(), 2)) + ').')
         
@@ -55,11 +71,21 @@ def report_exposure_time(data, ci = False):
     for lab in data['Modality Room'].unique():
         if ci:
             lci, uci = _calc_ci(data[data['Modality Room'] == lab]['F+A Time (s)'])
+        
+        median_min, median_sec = _format_min_sec(data[data['Modality Room'] == lab]['F+A Time (s)'].median())
+        
+        if ci:
+            lci_min, lci_sec = _format_min_sec(lci)
+            uci_min, uci_sec = _format_min_sec(uci)
+        
+        lIQR_min, lIQR_sec = _format_min_sec(data[data['Modality Room'] == lab]['F+A Time (s)'].quantile(0.25))
+        uIQR_min, uIQR_sec = _format_min_sec(data[data['Modality Room'] == lab]['F+A Time (s)'].quantile(0.75))
+        lrange_min, lrange_sec = _format_min_sec(data[data['Modality Room'] == lab]['F+A Time (s)'].min())
+        urange_min, urange_sec = _format_min_sec(data[data['Modality Room'] == lab]['F+A Time (s)'].max())
+
         print(lab + ': n = {:4}'.format(len(data[data['Modality Room'] == lab])) + \
-        ', Exposure time: Median - ' + str(round(data[data['Modality Room'] == lab]['F+A Time (s)'].median(), 2)) + ',' +\
-        (' 95% CI: [' + str(round(lci, 2)) + ' - ' + str(round(uci, 2)) + '] (s)' if ci else '') + \
+        ', Exposure time: Median - ' + median_min + ':' + median_sec + ' (min:s),' + \
+        (' 95% CI: [' + lci_min + ':' + lci_sec + ' - ' + uci_min + ':' + uci_sec + ']' if ci else '') + \
         # 25 th percentile:
-        ' IQR [' + str(round(data[data['Modality Room'] == lab]['F+A Time (s)'].quantile(0.25), 2)) + \
-        ' - ' + str(round(data[data['Modality Room'] == lab]['F+A Time (s)'].quantile(0.75), 2)) + '] (s), ' + \
-        'Range (' + str(round(data[data['Modality Room'] == lab]['F+A Time (s)'].min(), 2)) + \
-        ' - ' + str(round(data[data['Modality Room'] == lab]['F+A Time (s)'].max(), 2)) + ').')
+        ' IQR [' + lIQR_min + ':' + lIQR_sec + ' - ' + uIQR_min + ':' + uIQR_sec + '], ' + \
+        'Range (' + lrange_min + ':' + lrange_sec + ' - '  + urange_min + ':' + urange_sec + ').')
